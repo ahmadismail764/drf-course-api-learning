89edab9a599d7eb9808b95fa2f7c55ab2084a9c0
Ahmad Ismail <ahmedeldeehy30305@gmail.com>
2026-02-23 08:42:09 +0200

    Video 2: Add ProductSerializer for product validation and serialization
    - Created a new serializer for the Product model.
    - Included fields: id, name, price, and stock.
    - Added validation to ensure the price is greater than 0.

--- api/views.py
+++ api/views.py
@@ -1,1 +1,18 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from api.serializers import ProductSerializer
+from .models import Product
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from django.shortcuts import get_object_or_404
+
+@api_view(['GET'])
+def product_list(request):
+    products = Product.objects.all() # This returns a django model
+    serializer = ProductSerializer(products, many=True) # This turns it into a consumable format
+    return Response(serializer.data) # This returns the data as a JSON response
+
+@api_view(['GET'])
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    serializer = ProductSerializer(product) # This turns it into a consumable format
+    return Response(serializer.data) # This returns the data as a JSON response
