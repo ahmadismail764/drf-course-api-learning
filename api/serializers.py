18b8e07ca312d39dd7220724c1ded60650a0ba2a
Ahmad Ismail <ahmedeldeehy30305@gmail.com>
2026-02-23 22:32:09 +0200

    Video 3: Enhance serializers and views for Order and OrderItem; add order_list endpoint

--- api/serializers.py
+++ api/serializers.py
@@ -5,7 +5,6 @@ class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = ('id', 'name', 'price', 'stock')
-
     
     
     def validate_price(self, value):
@@ -14,3 +13,41 @@ class ProductSerializer(serializers.ModelSerializer):
                 "Price must be greater than 0."
             )
         return value
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    product_name = serializers.CharField(source='product.name')
+    product_price = serializers.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        source='product.price'
+    )
+    class Meta:
+        model = OrderItem
+        fields = (
+            'product_name',
+            'product_price',
+            'quantity',
+            'item_subtotal'
+        )
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    order_items = OrderItemSerializer(many=True, read_only=True)
+    # Read_only fields are not required for read or update or so
+    
+    total_price = serializers.SerializerMethodField()
+    def get_total_price(self, obj):
+        order_items = obj.order_items.all()
+        return sum(item.item_subtotal for item in order_items)
+    
+    class Meta:
+        model = Order
+        fields = (
+            'order_id',
+            'user',
+            'created_at',
+            'status',
+            'order_items',
+            'total_price'
+        )
+
--- api/views.py
+++ api/views.py
@@ -1,6 +1,6 @@
 from django.http import JsonResponse
-from api.serializers import ProductSerializer
-from .models import Product
+from api.serializers import ProductSerializer, OrderSerializer
+from .models import Product, Order, OrderItem
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from django.shortcuts import get_object_or_404
@@ -16,3 +16,10 @@ def product_detail(request, pk):
     product = get_object_or_404(Product, pk=pk)
     serializer = ProductSerializer(product) # This turns it into a consumable format
     return Response(serializer.data) # This returns the data as a JSON response
+
+@api_view(['GET'])
+def order_list(request):
+    orders = Order.objects.all() # This returns a django model
+    serializer = OrderSerializer(orders, many=True) # This turns it into a consumable format
+    return Response(serializer.data) # This returns the data as a JSON response
+
